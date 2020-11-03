5390b495bf18d386d883b1bfb4c8e44e5798e3c2
cjgmj <cjgmj@hotmail.com>
2020-11-03 16:07:14 +0100

    Booleans

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 "use strict";
 console.log("Hola mundo");
+// Booleans
+var esSuperman = true;
+var esBatman;
+var esIronman = true; // No recomendada
+var esSpiderman; // Interfaz
+if (esSuperman) {
+    console.log("Estamos salvados");
+}
+else {
+    console.log("Oops");
+}
+// esSuperman = false;
+esSuperman = convertirClark();
+function convertirClark() {
+    return false;
+}
--- app.ts
+++ app.ts
@@ -1,1 +1,20 @@
 console.log("Hola mundo");
+
+// Booleans
+let esSuperman: boolean = true;
+let esBatman: boolean;
+let esIronman = true; // No recomendada
+let esSpiderman: Boolean; // Interfaz
+
+if (esSuperman) {
+  console.log("Estamos salvados");
+} else {
+  console.log("Oops");
+}
+
+// esSuperman = false;
+esSuperman = convertirClark();
+
+function convertirClark() {
+  return false;
+}
