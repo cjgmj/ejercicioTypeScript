4c1a19ad0a46041ebc24403f905555a57f633e38
cjgmj <cjgmj@hotmail.com>
2020-11-03 16:11:00 +0100

    Numbers

--- app.js
+++ app.js
@@ -16,3 +16,18 @@ esSuperman = convertirClark();
 function convertirClark() {
     return false;
 }
+// Numbers
+// Soportan enteros y números decimales
+var avengers = 5;
+var villanos;
+var otros = 2; // No recomendada
+var xmen; // Interfaz
+villanos = 3;
+if (avengers > villanos) {
+    console.log("Estamos salvados");
+}
+else {
+    console.log("Oops");
+}
+otros = 123;
+otros = 123.123;
--- app.ts
+++ app.ts
@@ -18,3 +18,21 @@ esSuperman = convertirClark();
 function convertirClark() {
   return false;
 }
+
+// Numbers
+// Soportan enteros y números decimales
+let avengers: number = 5;
+let villanos: number;
+let otros = 2; // No recomendada
+let xmen: Number; // Interfaz
+
+villanos = 3;
+
+if (avengers > villanos) {
+  console.log("Estamos salvados");
+} else {
+  console.log("Oops");
+}
+
+otros = 123;
+otros = 123.123;
