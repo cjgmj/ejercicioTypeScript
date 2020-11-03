06de303e9141fa9f84f0f183379db05838fac666
cjgmj <cjgmj@hotmail.com>
2020-11-03 16:54:52 +0100

    Ejercicio 1

---
+++ ejercicios/ej1.js
@@ -0,0 +1,60 @@
+"use strict";
+// Base
+// // Tipos
+// var batmanEj = "Bruce";
+// var supermanEj = "Clark";
+// var existeEj = false;
+// // Tuplas
+// var parejaHeroes = [batman, superman];
+// var villano = ["Lex Lutor", 5, true];
+// // Arreglos
+// var aliados = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
+// //Enumeraciones
+// var fuerzaFlash = 5;
+// var fuerzaSuperman = 100;
+// var fuerzaBatman = 1;
+// var fuerzaAquaman = 0;
+// // Retorno de funciones
+// function activar_batiseñal() {
+//   return "activada";
+// }
+// function pedir_ayuda() {
+//   console.log("Auxilio!!!");
+// }
+// // Aserciones de Tipo
+// var poder = "100";
+// var largoDelPoder = poder.length;
+// console.log(largoDelPoder);
+// Solución
+// Tipos
+var batmanEj = "Bruce";
+var supermanEj = "Clark";
+var existeEj = false;
+// Tuplas
+var parejaHeroes = [batman, superman];
+var villano = ["Lex Lutor", 5, true];
+// Arreglos
+var aliados = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
+//Enumeraciones
+var Fuerza;
+(function (Fuerza) {
+    Fuerza[Fuerza["aquaman"] = 0] = "aquaman";
+    Fuerza[Fuerza["batman"] = 1] = "batman";
+    Fuerza[Fuerza["flash"] = 5] = "flash";
+    Fuerza[Fuerza["superman"] = 100] = "superman";
+})(Fuerza || (Fuerza = {}));
+var fuerzaFlash = Fuerza.flash;
+var fuerzaSuperman = Fuerza.superman;
+var fuerzaBatman = Fuerza.batman;
+var fuerzaAquaman = Fuerza.aquaman;
+// Retorno de funciones
+function activar_batiseñal() {
+    return "activada";
+}
+function pedir_ayuda() {
+    console.log("Auxilio!!!");
+}
+// Aserciones de Tipo
+var poder = "100";
+var largoDelPoder = poder.length;
+console.log(largoDelPoder);
---
+++ ejercicios/ej1.ts
@@ -0,0 +1,75 @@
+// Base
+
+// // Tipos
+// var batmanEj = "Bruce";
+// var supermanEj = "Clark";
+
+// var existeEj = false;
+
+// // Tuplas
+// var parejaHeroes = [batman, superman];
+// var villano = ["Lex Lutor", 5, true];
+
+// // Arreglos
+// var aliados = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
+
+// //Enumeraciones
+// var fuerzaFlash = 5;
+// var fuerzaSuperman = 100;
+// var fuerzaBatman = 1;
+// var fuerzaAquaman = 0;
+
+// // Retorno de funciones
+// function activar_batiseñal() {
+//   return "activada";
+// }
+
+// function pedir_ayuda() {
+//   console.log("Auxilio!!!");
+// }
+
+// // Aserciones de Tipo
+// var poder = "100";
+// var largoDelPoder = poder.length;
+// console.log(largoDelPoder);
+
+// Solución
+// Tipos
+let batmanEj: string = "Bruce";
+let supermanEj: string = "Clark";
+
+let existeEj: boolean = false;
+
+// Tuplas
+let parejaHeroes: [string, string] = [batman, superman];
+let villano: [string, number, boolean] = ["Lex Lutor", 5, true];
+
+// Arreglos
+let aliados: string[] = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
+
+//Enumeraciones
+enum Fuerza {
+  aquaman = 0,
+  batman = 1,
+  flash = 5,
+  superman = 100,
+}
+
+let fuerzaFlash = Fuerza.flash;
+let fuerzaSuperman = Fuerza.superman;
+let fuerzaBatman = Fuerza.batman;
+let fuerzaAquaman = Fuerza.aquaman;
+
+// Retorno de funciones
+function activar_batiseñal(): string {
+  return "activada";
+}
+
+function pedir_ayuda(): void {
+  console.log("Auxilio!!!");
+}
+
+// Aserciones de Tipo
+let poder = "100";
+let largoDelPoder: number = (<string>poder).length;
+console.log(largoDelPoder);
