739284896b072e36071670c8b3c161a722b50965
cjgmj <cjgmj@hotmail.com>
2020-11-04 21:10:21 +0100

    Objetos básicos

---
+++ objetos.ts
@@ -0,0 +1,15 @@
+// ********** Objetos básicos **********
+let flash = {
+  nombre: "Barry Allen",
+  edad: 24,
+  poderes: ["Velocidad", "Viajar en el tiempo"],
+};
+
+// flash.padres = []; // No podemos añadir nuevas propiedades
+// flash = {}; // No podemos definirlo como vacío porque espera las propiedades con las que se creó
+
+flash = {
+  nombre: "Clark Kent",
+  edad: 500,
+  poderes: ["Volar"],
+};
