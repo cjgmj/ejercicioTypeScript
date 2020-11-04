2b5fa2690d4e3d4b53aca9181e3f9d34ac04552c
cjgmj <cjgmj@hotmail.com>
2020-11-04 21:03:32 +0100

    Ejercicio 2

---
+++ ejercicios/ej2.js
@@ -0,0 +1,58 @@
+"use strict";
+// Base
+Object.defineProperty(exports, "__esModule", { value: true });
+// // Funciones Básicas
+// function sumar( a, b ){
+//     return a + b;
+//   }
+//   var contar = function( heroes ){
+//     return heroes.length;
+//   }
+//   var superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+//   contar(superHeroes);
+//   //Parametros por defecto
+//   function llamarBatman( llamar ){
+//     if( llamar ){
+//       console.log("Batiseñal activada");
+//     }
+//   }
+//   llamarBatman();
+//   // Rest?
+//   function unirheroes( personas ){
+//     return personas.join(", ");
+//   }
+//   // Tipo funcion
+//   function noHaceNada( numero, texto, booleano, arreglo ){
+//   }
+//   // Crear el tipo de funcion que acepte la funcion "noHaceNada"
+//   var noHaceNadaTampoco;
+// Solución
+// Funciones Básicas
+function sumar(a, b) {
+    return a + b;
+}
+var contar = function (heroes) {
+    return heroes.length;
+};
+var superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+contar(superHeroes);
+//Parametros por defecto
+function llamarBatman(llamar) {
+    if (llamar) {
+        console.log("Batiseñal activada");
+    }
+}
+llamarBatman();
+// Rest?
+function unirheroes() {
+    var personas = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        personas[_i] = arguments[_i];
+    }
+    return personas.join(", ");
+}
+// Tipo funcion
+function noHaceNada(numero, texto, booleano, arreglo) { }
+// Crear el tipo de funcion que acepte la funcion "noHaceNada"
+var noHaceNadaTampoco;
+noHaceNadaTampoco = noHaceNada;
