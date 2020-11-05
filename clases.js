b2dc149a06f7231d9284935eb8fd6b8b8f3d7da8
cjgmj <cjgmj@hotmail.com>
2020-11-05 17:32:30 +0100

    Métodos públicos, privados y protegidos

--- clases.js
+++ clases.js
@@ -17,7 +17,30 @@ var Avenger = /** @class */ (function () {
         this.equipo = equipo;
         this.nombreReal = nombreReal;
     }
+    // ********** Métodos públicos **********
+    // Si no se especifica por defecto son públicos
+    Avenger.prototype.bio = function () {
+        var mensaje = this.nombre + " " + this.nombreReal + " " + this.equipo;
+        console.log(mensaje);
+    };
+    Avenger.prototype.cambiaEquipoPublico = function (nuevoEquipo) {
+        return this.cambiarEquipo(nuevoEquipo); // Si no se pone el this busca una función dentro de este scope
+    };
+    // ********** Métodos protegidos **********
+    Avenger.prototype.sumarPeleaGanada = function () {
+        this.peleasGanadas++;
+    };
+    // ********** Métodos privados **********
+    Avenger.prototype.cambiarEquipo = function (nuevoEquipo) {
+        if (nuevoEquipo === this.equipo) {
+            return false;
+        }
+        this.equipo = nuevoEquipo;
+        return true;
+    };
     return Avenger;
 }());
 var antman = new Avenger("Antman", "cap", "Scott Lang");
+antman.bio();
+console.log(antman.cambiaEquipoPublico("cap"));
 console.log(antman);
--- clases.ts
+++ clases.ts
@@ -19,9 +19,37 @@ class Avenger {
     this.equipo = equipo;
     this.nombreReal = nombreReal;
   }
+  // ********** Métodos públicos **********
+  // Si no se especifica por defecto son públicos
+  public bio(): void {
+    let mensaje: string = `${this.nombre} ${this.nombreReal} ${this.equipo}`;
+
+    console.log(mensaje);
+  }
+
+  public cambiaEquipoPublico(nuevoEquipo: string): boolean {
+    return this.cambiarEquipo(nuevoEquipo); // Si no se pone el this busca una función dentro de este scope
+  }
+
+  // ********** Métodos protegidos **********
+  protected sumarPeleaGanada(): void {
+    this.peleasGanadas++;
+  }
+
+  // ********** Métodos privados **********
+  private cambiarEquipo(nuevoEquipo: string): boolean {
+    if (nuevoEquipo === this.equipo) {
+      return false;
+    }
+
+    this.equipo = nuevoEquipo;
+    return true;
+  }
 }
 
 let antman: Avenger = new Avenger("Antman", "cap", "Scott Lang");
+antman.bio();
+console.log(antman.cambiaEquipoPublico("cap"));
 
 console.log(antman);
 
