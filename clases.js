cb5b218f41ca212d32258aa9cddbab2236083a86
cjgmj <cjgmj@hotmail.com>
2020-11-05 17:15:18 +0100

    Clases básicas

---
+++ clases.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// ********** Clases básicas **********
+var Avenger = /** @class */ (function () {
+    function Avenger() {
+    }
+    return Avenger;
+}());
+var antman = new Avenger();
+console.log(antman);
---
+++ clases.ts
@@ -0,0 +1,15 @@
+// ********** Clases básicas **********
+class Avenger {
+  nombre: string;
+  equipo: string;
+  nombreReal: string;
+
+  puedePelear: boolean;
+  peleasGanadas: number;
+}
+
+let antman: Avenger = new Avenger();
+
+console.log(antman);
+
+export {};
